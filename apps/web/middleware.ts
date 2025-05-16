8ac304ab74f01b8860d924f592fd7eeb6297cea9
ajayramavath <ajayramavath03@gmail.com>
2025-05-16 06:29:18 +0530

    debug

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const isAuthenticated = !!token;
 
   const { pathname } = req.nextUrl;
