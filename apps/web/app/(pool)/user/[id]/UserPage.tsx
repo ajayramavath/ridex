40c08685e5b0eecb1508e6e6cffe4cd325365436
ajayramavath <ajayramavath03@gmail.com>
2025-06-10 20:14:57 +0530

    build issues

--- apps/web/app/(pool)/user/[id]/UserPage.tsx
+++ apps/web/app/(pool)/user/[id]/UserPage.tsx
@@ -2,7 +2,6 @@
 import PostedRide from '@/components/Profile/PostedRide';
 import { useGetUserByIdQuery } from '@/redux/user/userApi'
 import { Avatar, AvatarFallback, AvatarImage } from '@ridex/ui/components/avatar';
-import { ScrollArea } from '@ridex/ui/components/scroll-area';
 import { Separator } from '@ridex/ui/components/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@ridex/ui/components/tabs';
 import { Car, ListCheck, Loader2Icon, Settings, ThumbsUp, User2Icon, Users } from 'lucide-react';
@@ -41,95 +40,92 @@ const UserPage = ({ id }: { id: string }) => {
   }
 
   return (
-    <ScrollArea className='w-full h-screen'>
-      <div className={cn('flex flex-col justify-between gap-y-4', isMobile ? 'py-8 px-4' : '')}>
-        <div className='flex flex-col md:flex-row gap-y-4 justify-around md:items-center'>
-          <div className='flex gap-x-4 md:justify-center pt-8 items-center w-[40%]'>
-            <Avatar className="md:h-32 md:w-32 w-16 h-16">
-              <AvatarImage src={user?.profile_photo ? user.profile_photo : undefined} alt="Profile" />
-              <AvatarFallback className="text-2xl bg-red-400" delayMs={0}>
-                <User2Icon />
-              </AvatarFallback>
-            </Avatar>
-            <div className='space-y-2'>
-              <div className='text-xl font-bold'>{user?.name}</div>
-              <div>{user?.bio}</div>
-            </div>
-          </div>
-          <div className='flex gap-x-4 text-muted-foreground md:gap-x-8 items-center'>
-            <div className='flex flex-col md:text-sm text-xs'>
-              Rides Posted
-              <span className='md:text-xl font-bold'>{user?.posted_rides?.length}</span>
-            </div>
-            <div className='flex flex-col md:text-sm text-xs'>
-              Rides Joined
-              <span className='md:text-xl font-bold'>{user?.joined_rides?.length}</span>
-            </div>
-            <div className='flex flex-col md:text-sm text-xs'>
-              Rating
-              <span className='md:text-xl font-bold'>{`${getUserRating(user)}`}</span>
-            </div>
+    <div className={cn('flex flex-col justify-between gap-y-4 w-full h-full', isMobile ? 'py-8 px-4' : '')}>
+      <div className='flex flex-col md:flex-row gap-y-4 justify-around md:items-center'>
+        <div className='flex gap-x-4 md:justify-center pt-8 items-center w-[40%]'>
+          <Avatar className="md:h-32 md:w-32 w-16 h-16">
+            <AvatarImage src={user?.profile_photo ? user.profile_photo : undefined} alt="Profile" />
+            <AvatarFallback className="text-2xl bg-red-400" delayMs={0}>
+              <User2Icon />
+            </AvatarFallback>
+          </Avatar>
+          <div className='space-y-2'>
+            <div className='text-xl font-bold'>{user?.name}</div>
+            <div>{user?.bio}</div>
           </div>
         </div>
-        <div>
-          <Tabs defaultValue="ridesPosted" className="w-full md:px-8 py-4">
-            <TabsList className='bg-background md:px-2'>
-              <TabsTrigger
-                value="ridesPosted"
-                className={cn(TabsTriggerClassName)}
-              >
-                <ListCheck className='w-5 h-5' />
-                <span className='hidden md:block'>Rides Posted</span>
-              </TabsTrigger>
-              <TabsTrigger
-                value="ridesJoined"
-                className={cn(TabsTriggerClassName)}
-              >
-                <Users className='w-5 h-5' />
-                <span className='hidden md:block'>Rides Joined</span>
-              </TabsTrigger>
-              <TabsTrigger value="preferences" className={cn(TabsTriggerClassName)}>
-                <Settings className='w-5 h-5' />
-                <span className='hidden md:block'>Preferences</span>
-              </TabsTrigger>
-              <TabsTrigger value="reviews" className={cn(TabsTriggerClassName)}>
-                <ThumbsUp className='w-5 h-5' />
-                <span className='hidden md:block'>Reviews</span>
-              </TabsTrigger>
-              <TabsTrigger value="vehicles" className={cn(TabsTriggerClassName)}>
-                <Car className='w-5 h-5' />
-                <span className='hidden md:block'>Vehicles</span>
-              </TabsTrigger>
-            </TabsList>
-            <Separator className='bg-muted-foreground' />
-            <TabsContent value="ridesPosted" className='h-full'>
-              <h1 className='text-sm font-bold'>Rides Posted</h1>
-              {user.posted_rides.length === 0 && <div className='text-muted-foreground'>No rides yet</div>}
-              {user.posted_rides.map(ride => {
-                return <PostedRide key={ride.id} ride={ride} />
-              })}
-            </TabsContent>
-            <TabsContent value="ridesJoined" className='h-full'>
-              <h1 className='text-sm font-bold mb-4'>Rides Joined</h1>
-              {user.joined_rides.length === 0 && <div className='text-muted-foreground'>No rides yet</div>}
-              {user.joined_rides.map(joined => {
-                return <JoinedRides key={joined.id} passenger={joined} />
-              })}
-            </TabsContent>
-            <TabsContent value="reviews" className='h-full'>
-              <ReviewSection user={user} />
-            </TabsContent>
-            <TabsContent value="preferences" className='h-full'>
-              <PreferanceSection user={user} />
-            </TabsContent>
-            <TabsContent value="vehicles" className='h-full'>
-              <VehiclesSection user={user} />
-            </TabsContent>
-          </Tabs>
+        <div className='flex gap-x-4 text-muted-foreground md:gap-x-8 items-center'>
+          <div className='flex flex-col md:text-sm text-xs'>
+            Rides Posted
+            <span className='md:text-xl font-bold'>{user?.posted_rides?.length}</span>
+          </div>
+          <div className='flex flex-col md:text-sm text-xs'>
+            Rides Joined
+            <span className='md:text-xl font-bold'>{user?.joined_rides?.length}</span>
+          </div>
+          <div className='flex flex-col md:text-sm text-xs'>
+            Rating
+            <span className='md:text-xl font-bold'>{`${getUserRating(user)}`}</span>
+          </div>
         </div>
-      </div >
-    </ScrollArea >
-
+      </div>
+      <div>
+        <Tabs defaultValue="ridesPosted" className="w-full md:px-8 py-4">
+          <TabsList className='bg-background md:px-2'>
+            <TabsTrigger
+              value="ridesPosted"
+              className={cn(TabsTriggerClassName)}
+            >
+              <ListCheck className='w-5 h-5' />
+              <span className='hidden md:block'>Rides Posted</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="ridesJoined"
+              className={cn(TabsTriggerClassName)}
+            >
+              <Users className='w-5 h-5' />
+              <span className='hidden md:block'>Rides Joined</span>
+            </TabsTrigger>
+            <TabsTrigger value="preferences" className={cn(TabsTriggerClassName)}>
+              <Settings className='w-5 h-5' />
+              <span className='hidden md:block'>Preferences</span>
+            </TabsTrigger>
+            <TabsTrigger value="reviews" className={cn(TabsTriggerClassName)}>
+              <ThumbsUp className='w-5 h-5' />
+              <span className='hidden md:block'>Reviews</span>
+            </TabsTrigger>
+            <TabsTrigger value="vehicles" className={cn(TabsTriggerClassName)}>
+              <Car className='w-5 h-5' />
+              <span className='hidden md:block'>Vehicles</span>
+            </TabsTrigger>
+          </TabsList>
+          <Separator className='bg-muted-foreground' />
+          <TabsContent value="ridesPosted" className='h-full'>
+            <h1 className='text-sm font-bold'>Rides Posted</h1>
+            {user.posted_rides.length === 0 && <div className='text-muted-foreground'>No rides yet</div>}
+            {user.posted_rides.map(ride => {
+              return <PostedRide key={ride.id} ride={ride} />
+            })}
+          </TabsContent>
+          <TabsContent value="ridesJoined" className='h-full'>
+            <h1 className='text-sm font-bold mb-4'>Rides Joined</h1>
+            {user.joined_rides.length === 0 && <div className='text-muted-foreground'>No rides yet</div>}
+            {user.joined_rides.map(joined => {
+              return <JoinedRides key={joined.id} passenger={joined} />
+            })}
+          </TabsContent>
+          <TabsContent value="reviews" className='h-full'>
+            <ReviewSection user={user} />
+          </TabsContent>
+          <TabsContent value="preferences" className='h-full'>
+            <PreferanceSection user={user} />
+          </TabsContent>
+          <TabsContent value="vehicles" className='h-full'>
+            <VehiclesSection user={user} />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div >
   )
 }
 
